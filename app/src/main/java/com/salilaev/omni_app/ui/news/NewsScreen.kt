1074e4e40b007ccf828f5c2b9d69aae47dfcbcbf
salilaev <salilaev02@gmail.com>
2025-04-06 19:55:10 +0500

    .

--- app/src/main/java/com/salilaev/omni_app/ui/news/NewsScreen.kt
+++ app/src/main/java/com/salilaev/omni_app/ui/news/NewsScreen.kt
@@ -253,28 +253,5 @@ class NewsScreen : ComposeFragment() {
     }
 }
 
-fun formatPublishedDateLegacy(dateString: String?): String {
-    if (dateString.isNullOrBlank()) return ""
-    return try {
-        val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
-        inputFormat.timeZone = TimeZone.getTimeZone("UTC")
-        val date = inputFormat.parse(dateString) ?: return ""
-
-        val outputFormat = SimpleDateFormat("d MMMM yyyy, HH:mm", Locale("eng"))
-        outputFormat.format(date)
-    } catch (e: Exception) {
-        ""
-    }
-}
-
-fun formatAuthor(author: String?): String {
-    val maxAuthorLength = 30
-    return if ((author?.length ?: 0) > maxAuthorLength) {
-        author?.take(maxAuthorLength) + "..."
-    } else {
-        author ?: ""
-    }
-}
-
 
 
